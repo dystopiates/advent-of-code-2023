9c35d6c6795a6b2c13ce1268b114a27d0b30da29
dystopiates <67548276+dystopiates@users.noreply.github.com>
2023-12-07 10:11:20 -0800

    Fixed day7

--- day7.py
+++ day7.py
@@ -5,32 +5,33 @@ from utils import readlines, maplinetypes
 
 def grade_hand_type(hand: str, wild_jokers: bool) -> str:
     if wild_jokers:
-        hand = hand.replace('J', Counter(hand).most_common()[0][0])
+        replacement = Counter(hand.replace('J', '').ljust(1, '_')).most_common()[0][0]
+        hand = hand.replace('J', replacement)
 
-    counts = [c[1] for c in Counter(hand).most_common()]
+    counts = [str(c[1]) for c in Counter(hand).most_common()]
 
     return ''.join(counts).ljust(5, '0')
 
 
 def generate_hand_value(hand: str, wild_jokers: bool, strengths: str) -> str:
-    return grade_hand_type(hand, wild_jokers) + ''.join('0123456789ABCDEF'[strengths.rindex(c)] for c in hand)
+    return grade_hand_type(hand, wild_jokers) + ''.join('0123456789ABC'[strengths.index(c)] for c in hand)
 
 
 def play(lines: list[str], wild_jokers: bool, strengths: str):
     hand_bids = maplinetypes(lines, str, int)
-    ranked = [generate_hand_value(hand, wild_jokers, strengths) for hand, _ in hand_bids]
+    ranked = [(generate_hand_value(hand, wild_jokers, strengths), bid) for hand, bid in hand_bids]
     ranked.sort(key=lambda r: r[0])
 
     return sum((i + 1) * bid for i, (_, bid) in enumerate(ranked))
 
 
 def part1(lines: list[str]):
-    print(play(lines, False, 'AKQJT98765432'))
+    print(play(lines, False, 'AKQJT98765432'[::-1]))
 
 
 def part2(lines: list[str]):
-    print(play(lines, True, 'AKQT98765432J'))
+    print(play(lines, True, 'AKQT98765432J'[::-1]))
 
 
 if __name__ == '__main__':
-    part1(readlines('day7.test.input'))
+    part2(readlines('day7.input'))
